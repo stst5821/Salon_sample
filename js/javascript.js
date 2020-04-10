5a95594f71ea8ac9cae108f2a2d4f676feea0e70
stst5821 <stst5821@gmail.com>
2020-04-10 16:18:47 +0900

    スムーススクロール、ヘッダーメニュークリックすると赤線追加

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,28 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     }
 });
+
+
+// ヘッダーメニューのスムーススクロール
+$(function () {
+    var headerHight = 100; // ヘッダの高さを指定する
+    $('.nav_btn').click(function () {
+        //↑ aリンクのclassを指定
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - headerHight; //ヘッダの高さ分位置をずらす
+        $("html, body").animate({ scrollTop: position }, 550, "swing");
+    });
+});
+
+// ヘッダーメニューをクリックしたら下線を引く
+$('.nav_item').click(function () {
+    var $answer = $(this).find('.nav_btn');
+  
+    if ($answer.hasClass('active')) {
+      $answer.removeClass('active');
+    } else {
+      $('.nav_btn').removeClass('active');
+      $answer.addClass('active');
+    }
+  });
