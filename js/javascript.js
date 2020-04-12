e9b7563e9158dad7aca7885f9163c45c8033d7f9
stst5821 <stst5821@gmail.com>
2020-04-12 15:13:03 +0900

    カルーセル自動化、ヘッダーメニューの下線色変え

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,55 @@
 var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+    reverseDirection: false
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  speed: 600,
 });
 
 
 // ヘッダーメニューのスムーススクロール
 $(function () {
-    var headerHight = 100; // ヘッダの高さを指定する
-    $('.nav_btn').click(function () {
-        //↑ aリンクのclassを指定
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top - headerHight; //ヘッダの高さ分位置をずらす
-        $("html, body").animate({ scrollTop: position }, 550, "swing");
-    });
+  var headerHight = 100; // ヘッダの高さを指定する
+  $('.nav_btn').click(function () {
+    //↑ aリンクのclassを指定
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - headerHight; //ヘッダの高さ分位置をずらす
+    $("html, body").animate({ scrollTop: position }, 550, "swing");
+  });
 });
 
 // ヘッダーメニューをクリックしたら下線を引く
 $('.nav_item').click(function () {
-    var $answer = $(this).find('.nav_btn');
-  
-    if ($answer.hasClass('active')) {
-      $answer.removeClass('active');
-    } else {
-      $('.nav_btn').removeClass('active');
-      $answer.addClass('active');
-    }
-  });
+  var $answer = $(this).find('.nav_btn');
+
+  if ($answer.hasClass('active')) {
+    $answer.removeClass('active');
+  } else {
+    $('.nav_btn').removeClass('active');
+    $answer.addClass('active');
+  }
+});
+
+// TOPへ戻るボタン 画面右下の
+jQuery(window).on("scroll", function ($) {
+  if (jQuery(this).scrollTop() > 100) {
+    jQuery('.floating').show();
+  } else {
+    jQuery('.floating').hide();
+  }
+});
+
+jQuery('.floating').click(function () {
+  jQuery('body,html').animate({
+    scrollTop: 0
+  }, 500);
+  return false;
+});
